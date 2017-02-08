6255f8be31bb3b8a269a23a14c6d21e7918e1b93
nturner5 <nturner5@users.noreply.github.com>
2017-02-08 13:49:51 -0700

    origin master

--- controller.js
+++ controller.js
@@ -1,10 +1,42 @@
+// angular.module('userProfiles').controller('MainController', function ($scope, mainService) {
+
+//   $scope.getUsers = function () {
+//     mainService.getUsers().then(function (response) {
+//       $scope.users = response.data.data;
+//     });
+//   };
+
+
+//   $scope.getUsers();
+
+// });
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+ $scope.currentPage = 2;
+
+ $scope.prev = function(){
+if($scope.currentPage >1){
+$scope.currentPage--;
+$scope.getUsers(); 
+}
+ }
+ 
+ $scope.next = function(){
+   if($scope.currentPage < $scope.total_pages){
+$scope.currentPage++;
+ $scope.getUsers();
+}
+ }
+ 
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+      var promise = mainService.getUsers($scope.currentPage);
+    promise.then(function(usersObj){
+        $scope.users = usersObj.data;
+        $scope.total_pages = usersObj.total_pages;
+    })
   }
 
 
-  $scope.getUsers();
 
+  $scope.getUsers();
 });
